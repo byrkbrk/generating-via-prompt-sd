faa1c8cc3b7b1135f6df52d0b2730ea108b8a736
burak <byrkbrk@gmail.com>
2024-07-13 13:25:36 +0300

    upload app file

---
+++ app.py
@@ -0,0 +1,19 @@
+from stable_diffusion import StableDiffusion
+import gradio as gr
+
+
+
+if __name__ == "__main__":
+    stable_diffusion = StableDiffusion(create_dirs=False)
+    gr_interface = gr.Interface(
+        fn=lambda prompts, save=False, show=False: stable_diffusion.generate(prompts.split("\n"),
+                                                                             save,
+                                                                             show)[0],
+        inputs=[gr.Textbox(lines=3, placeholder="an image of turtle in Camille Pissarro style")],
+        outputs=gr.Image(type="pil")
+    )
+    gr_interface.launch()
+
+
+
+
